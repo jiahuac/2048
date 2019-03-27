887b29ac32d20f371daba2d6d141c559d03c9c83
Jiahua Chen <jc@jh.tc>
2019-03-27 12:01:36 -0400

    Movement coordinates

---
+++ src/Move.java
@@ -0,0 +1,8 @@
+public class Move {
+    public static final int[] UP = {0, 0, 1};
+    public static final int[] DOWN = {0, 0, -1};
+    public static final int[] LEFT = {0, -1, 0};
+    public static final int[] RIGHT = {0, 1, 0};
+    public static final int[] FORWARD = {1, 0, 0};
+    public static final int[] BACKWARD = {-1, 0, 0};
+}
